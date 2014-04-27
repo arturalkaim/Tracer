f25948853e33ea5823d202ba7f2276f92081cd9a
arturalkaim <arturalkaim@gmail.com>
2014-04-27 22:06:51 +0100

    First Commit

---
+++ Trace-PAv/src/ist/meic/pa/TraceTranslator.java
@@ -0,0 +1,22 @@
+package ist.meic.pa;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.NotFoundException;
+import javassist.Translator;
+
+public class TraceTranslator implements Translator {
+
+	@Override
+	public void onLoad(ClassPool arg0, String arg1) throws NotFoundException,
+			CannotCompileException {
+
+	}
+
+	@Override
+	public void start(ClassPool arg0) throws NotFoundException,
+			CannotCompileException {
+
+	}
+
+}
---
+++ Trace-PAv/src/ist/meic/pa/TraceVM.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package ist.meic.pa;
+
+import javassist.*;
+import javassist.expr.*;
+
+import java.io.*;
+import java.lang.reflect.*;
+
+
+/**
+ * @author artur
+ * 
+ */
+public class TraceVM {
+
+	/**
+	 * @param args
+	 * @throws Throwable 
+	 */
+	public static void main(String[] args) throws Throwable {
+		if (args.length < 1) {
+		} else {
+			Translator translator = new TraceTranslator();
+			ClassPool pool = ClassPool.getDefault();
+			Loader classLoader = new Loader();
+			classLoader.addTranslator(pool, translator);
+			String[] restArgs = new String[args.length - 1];
+			System.arraycopy(args, 1, restArgs, 0, restArgs.length);
+			classLoader.run(args[0], restArgs);
+		}
+
+	}
+}
