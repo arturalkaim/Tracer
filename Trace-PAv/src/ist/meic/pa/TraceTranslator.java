3872fdf354c3d0f4267837dd148650699243f932
arturalkaim <arturalkaim@gmail.com>
2014-04-27 22:27:38 +0100

    Print lines

--- Trace-PAv/src/ist/meic/pa/TraceTranslator.java
+++ Trace-PAv/src/ist/meic/pa/TraceTranslator.java
@@ -1,15 +1,30 @@
 package ist.meic.pa;
 
-import javassist.CannotCompileException;
-import javassist.ClassPool;
-import javassist.NotFoundException;
-import javassist.Translator;
+import javassist.*;
+import javassist.expr.*;
+
+import java.io.*;
+import java.lang.reflect.*;
 
 public class TraceTranslator implements Translator {
 
 	@Override
-	public void onLoad(ClassPool arg0, String arg1) throws NotFoundException,
-			CannotCompileException {
+	public void onLoad(ClassPool pool, String className)
+			throws NotFoundException, CannotCompileException {
+		CtClass ctClass = pool.get(className);
+		makeTracable(ctClass);
+
+	}
+
+	private void makeTracable(CtClass ctClass) throws CannotCompileException {
+		
+		for (CtMethod ctMethod : ctClass.getDeclaredMethods()) {
+			ctMethod.instrument(new ExprEditor() {
+				public void edit(NewExpr newEx) throws CannotCompileException {
+					System.out.println(newEx.getClassName() + " " +  newEx.getFileName() +" at line: " + newEx.getLineNumber());
+				}
+			});
+		}
 
 	}
 
