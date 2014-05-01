0c2bb115fb0fb83b1e5055326c520f7390866f0a
arturalkaim <arturalkaim@gmail.com>
2014-05-01 19:56:14 +0100

    Added new Array

--- Trace-PAv/src/Test2.java
+++ Trace-PAv/src/Test2.java
@@ -12,13 +12,14 @@ class Test {
 
 	public void test() {
 		Object o = new String("MyObj");
-
+		Object[] arr= new Object[1];
+		arr[0]=o;
 		identity(o);
 		m.put(2, o);
 		m.get(2);
 
-		Trace.print(o);
-
+		Trace.print(arr);
+		Trace.print(arr[0]);
 		for (Object obj : m.values()) {
 			System.out.println(obj);
 		}
